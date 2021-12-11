dd09d3a66d25e365d2d45ff32df99ae1d7a6c333
Dan Levings <dan.levings@cognite.com>
2021-12-11 12:38:49 +0100

    day 11

--- 2021/day-11.js
+++ 2021/day-11.js
@@ -1,13 +1,83 @@
 const data = (type = "") => {
-  return require("./input")(__filename, "\n", type);
+  return require("./input")(__filename, "\n", type).map(x => x.split('').map(Number));
 };
+let flashed = {};
+const flash = (data, row, col) => {
+  flashed[`${row},${col}`] = true;
+  data[row][col] = 0;
+  up(data, row - 1, col);
+  up(data, row + 1, col);
+  up(data, row, col - 1);
+  up(data, row, col + 1);
+  up(data, row - 1, col - 1);
+  up(data, row + 1, col + 1);
+  up(data, row - 1, col + 1);
+  up(data, row + 1, col - 1);
+}
+
+const up = (data, row, col) => {
+  let cell = (data[row] || [])[col];
+  if (!cell ){
+    return;
+  }
+  data[row][col] += 1;
+  
+  if (data[row][col] > 9 && !flashed[`${row},${col}`]) {
+    flash(data, row, col);
+    return;
+  }
+}
+
+const step = (data) => {
+  for(let row = 0; row < data.length; row++) {
+    for(let col = 0; col < data[row].length; col++) {
+      data[row][col] += 1;
+    }
+  }
+  for(let row = 0; row < data.length; row++) {
+    for(let col = 0; col < data[row].length; col++) {
+      if (data[row][col] > 9) {
+        flash(data, row, col)
+      }
+    }
+  }
+  // Object.keys(flashed).forEach(key => {
+  //   let [row, col] = key.split(',').map(Number);
+  //   data[row][col] = 0;
+  // });
+}
+
+const print = (data) => {
+  console.log(data.map(x => x.join('')).join('\n'));
+}
 
 const part1 = (data) => {
-  return undefined;
+  let flashCount = 0;
+  for(let i = 0; i < 100; i++) {
+    console.log(i);
+    step(data);
+    print(data);
+    flashCount += Object.values(flashed).length;
+    flashed = {};
+  }
+
+  return flashCount;
 };
 
 const part2 = (data) => {
-  return undefined;
+  let found = false;
+  let count = 0;
+  while(!found) {
+    count++;
+    step(data);
+    let flashCount = Object.values(flashed).length;
+    flashed = {};
+    if (flashCount === 100) {
+      found = true;
+    }
+  }
+
+  return count;
 };
 
 /* istanbul ignore next */
--- 2021/day-11.spec.js
+++ 2021/day-11.spec.js
@@ -7,21 +7,21 @@ const {
 describe("Day 11", () => {
   describe("Part 1", () => {
     it("works for test data", () => {
-      expect(part1(data("test"))).toBe(undefined);
+      expect(part1(data("test"))).toBe(1656);
     });
 
     it("works for real data", () => {
-      expect(part1(data())).toBe(undefined);
+      expect(part1(data())).toBe(1588);
     });
   });
 
   describe("Part 2", () => {
     it("works for test data", () => {
-      expect(part2(data("test"))).toBe(undefined);
+      expect(part2(data("test"))).toBe(195);
     });
 
     it("works for real data", () => {
-      expect(part2(data())).toBe(undefined);
+      expect(part2(data())).toBe(517);
     });
   });
 });
